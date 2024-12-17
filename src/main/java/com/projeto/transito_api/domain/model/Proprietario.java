c690faa08325ae7b7266200437ed600a099aea2c
luanmvcosta0 <luanmvcosta@mgail.com>
2024-12-17 20:08:44 -0300

    Mapeando a entidade proprietario

--- src/main/java/com/projeto/transito_api/domain/model/Proprietario.java
+++ src/main/java/com/projeto/transito_api/domain/model/Proprietario.java
@@ -1,12 +1,20 @@
 package com.projeto.transito_api.domain.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_proprietario")
 public class Proprietario {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
