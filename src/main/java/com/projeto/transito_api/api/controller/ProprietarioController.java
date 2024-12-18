0e53db76951daa73f73e9fd1de20b9bb1d5b373b
luanmvcosta0 <luanmvcosta@mgail.com>
2024-12-18 19:20:07 -0300

    Usando o repositorio ProprietarioRepository para usar o metodo findAll

--- src/main/java/com/projeto/transito_api/api/controller/ProprietarioController.java
+++ src/main/java/com/projeto/transito_api/api/controller/ProprietarioController.java
@@ -1,10 +1,12 @@
 package com.projeto.transito_api.api.controller;
 
 import com.projeto.transito_api.entities.Proprietario;
+import com.projeto.transito_api.repository.ProprietarioRepository;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,13 +16,12 @@ import java.util.List;
 @RestController
 public class ProprietarioController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    @Autowired
+    private ProprietarioRepository proprietarioRepository;
 
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
-        TypedQuery<Proprietario> query = manager.createQuery("from Proprietario", Proprietario.class);
-        return query.getResultList();
+        return proprietarioRepository.findAll();
     }
 
 }
