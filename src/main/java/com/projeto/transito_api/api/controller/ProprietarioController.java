10d2375907e3746c3daed77f4f09195d024285dc
luanmvcosta0 <luanmvcosta@mgail.com>
2024-12-18 19:54:06 -0300

    Utilizando os query metods

--- src/main/java/com/projeto/transito_api/api/controller/ProprietarioController.java
+++ src/main/java/com/projeto/transito_api/api/controller/ProprietarioController.java
@@ -2,15 +2,9 @@ package com.projeto.transito_api.api.controller;
 
 import com.projeto.transito_api.entities.Proprietario;
 import com.projeto.transito_api.repository.ProprietarioRepository;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.TypedQuery;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,7 +13,7 @@ import java.util.List;
 public class ProprietarioController {
 
 
-    //@Autowired                                                // --> É boa pratica utilizar contrutor para instanciar
+    //@Autowired                                                // --> É boa pratica utilizar contrutor para instanciar (
     //private ProprietarioRepository proprietarioRepository;    // --> É boa pratica utilizar contrutor para instanciar
 
 
@@ -29,5 +23,16 @@ public class ProprietarioController {
     public List<Proprietario> listar() {
         return proprietarioRepository.findAll();
     }
+
+    @GetMapping("/proprietariosNomeCompleto")
+    public List<Proprietario> listarPorNome() {
+        return proprietarioRepository.findByNome("Luan Costa");
+    }
+
+    @GetMapping("/proprietariosLetra")
+    public List<Proprietario> listarPorLetra() {
+        return proprietarioRepository.findByNomeContaining("Bu");
+    }
+
 
 }
--- src/main/java/com/projeto/transito_api/repository/ProprietarioRepository.java
+++ src/main/java/com/projeto/transito_api/repository/ProprietarioRepository.java
@@ -4,7 +4,12 @@ import com.projeto.transito_api.entities.Proprietario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProprietarioRepository extends JpaRepository<Proprietario, Long> {
+
+    List<Proprietario> findByNome(String nome);
+    List<Proprietario> findByNomeContaining(String nome);   //O que passar aqui vai puxar por alguma parte do nome, por conta deste 'Containing'
 
 }
