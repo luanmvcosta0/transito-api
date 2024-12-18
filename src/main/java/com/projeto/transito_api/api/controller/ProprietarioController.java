f133c58c8e42ffb807d6cd34266cf9a8451643de
luanmvcosta0 <luanmvcosta@mgail.com>
2024-12-18 19:37:56 -0300

    Utilizando o construtor para instanciar o proprietarioRepository pois Ãe uma boa pratica, utilizei a anotation @AllArgsConstructor, que gera automaticamente um construtor que aceita um argumento para cada campo declarado na classe

--- src/main/java/com/projeto/transito_api/api/controller/ProprietarioController.java
+++ src/main/java/com/projeto/transito_api/api/controller/ProprietarioController.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 public class ProprietarioController {
 
-    @Autowired
-    private ProprietarioRepository proprietarioRepository;
+
+    //@Autowired                                                // --> É boa pratica utilizar contrutor para instanciar
+    //private ProprietarioRepository proprietarioRepository;    // --> É boa pratica utilizar contrutor para instanciar
+
+
+    private final ProprietarioRepository proprietarioRepository;
 
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
