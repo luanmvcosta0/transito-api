59ea1fb736c77bf72abce04b2a8972ff5aa5aabc
luanmvcosta0 <luanmvcosta@mgail.com>
2025-01-22 21:05:27 -0300

    Implementando o endpoint de inclusao de recurso

--- src/main/java/com/projeto/transito_api/api/controller/ProprietarioController.java
+++ src/main/java/com/projeto/transito_api/api/controller/ProprietarioController.java
@@ -3,45 +3,42 @@ package com.projeto.transito_api.api.controller;
 import com.projeto.transito_api.entities.Proprietario;
 import com.projeto.transito_api.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/proprietarios")
 public class ProprietarioController {
 
 
-    //@Autowired                                                // --> É boa pratica utilizar contrutor para instanciar (
+    //@Autowired                                                // --> É boa pratica utilizar contrutor para instanciar
     //private ProprietarioRepository proprietarioRepository;    // --> É boa pratica utilizar contrutor para instanciar
 
 
     private final ProprietarioRepository proprietarioRepository;
 
-    @GetMapping("/proprietarios")
+    @GetMapping("")
     public List<Proprietario> listar() {
         return proprietarioRepository.findAll();
     }
 
-    @GetMapping("/proprietariosNomeCompleto")
-    public List<Proprietario> listarPorNome() {
-        return proprietarioRepository.findByNome("Luan Costa");
-    }
-
-    @GetMapping("/proprietariosLetra")
-    public List<Proprietario> listarPorLetra() {
-        return proprietarioRepository.findByNomeContaining("Bu");
-    }
-
-    @GetMapping(value = "/proprietarios/{proprietarioId}")
+    @GetMapping(value = "/{proprietarioId}")
     public ResponseEntity<Proprietario> buscar(@PathVariable Long proprietarioId) {
         return proprietarioRepository.findById(proprietarioId)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Proprietario adicionar(@RequestBody Proprietario proprietario) {
+        return proprietarioRepository.save(proprietario);
     }
 
 }
