5761ea42b147712faa998a743fe124512d7e8d6d
luanmvcosta0 <luanmvcosta@mgail.com>
2025-01-30 21:07:52 -0300

    Adicionando tratamento para excecao de proprietario nao encontrado

--- src/main/java/com/projeto/transito_api/service/RegistroVeiculoService.java
+++ src/main/java/com/projeto/transito_api/service/RegistroVeiculoService.java
@@ -1,8 +1,10 @@
 package com.projeto.transito_api.service;
 
+import com.projeto.transito_api.entities.Proprietario;
 import com.projeto.transito_api.entities.StatusVeiculo;
 import com.projeto.transito_api.entities.Veiculo;
 import com.projeto.transito_api.exception.NegocioExeption;
+import com.projeto.transito_api.repository.ProprietarioRepository;
 import com.projeto.transito_api.repository.VeiculoRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,6 +17,7 @@ import java.time.LocalDateTime;
 public class RegistroVeiculoService {
 
     private final VeiculoRepository veiculoRepository;
+    private final ProprietarioRepository proprietarioRepository;
 
     @Transactional
     public Veiculo cadastrar(Veiculo novoVeiculo) {
@@ -30,8 +33,13 @@ public class RegistroVeiculoService {
             throw new NegocioExeption("Já existe um veiculo cadastrado com esta placa.");
         }
 
+        Proprietario proprietario = proprietarioRepository.findById(novoVeiculo.getProprietario().getId())
+                        .orElseThrow(() -> new NegocioExeption("Proprietario não encontrado"));
+
+        novoVeiculo.setProprietario(proprietario);
         novoVeiculo.setStatus(StatusVeiculo.REGULAR);
         novoVeiculo.setDataCadastro(LocalDateTime.now());
+
 
         return veiculoRepository.save(novoVeiculo);
     }
