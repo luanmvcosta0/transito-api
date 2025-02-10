383a19395d0c342b306fd17cfaef486f37755125
luanmvcosta0 <luanmvcosta@gmail.com>
2025-02-10 19:07:34 -0300

    Adicionando validacoes na entidade veiculo

--- src/main/java/com/projeto/transito_api/entities/Veiculo.java
+++ src/main/java/com/projeto/transito_api/entities/Veiculo.java
@@ -2,6 +2,9 @@ package com.projeto.transito_api.entities;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,12 +22,19 @@ public class Veiculo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "proprietario_id")
     private Proprietario proprietario;
 
+    @NotBlank
     private String marca;
+
+    @NotBlank
     private String modelo;
+
+    @NotBlank
+    @Pattern(regexp = "[A-Z]{3}[0-9][0-9A-Z][0-9]{2}")
     private String placa;
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)    //Para deixar essa propriedade com somente leitura, ou seja, so pode ser lido, não pode ser enviado no JSON
